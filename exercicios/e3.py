f4e93af19702dc05d0f800284d6f9ff36446a8b5
cassiano <cgrsobral@gmail.com>
2024-11-05 12:23:02 -0300

    fiz mais 4 exercicios

--- exercicios/e3.py
+++ exercicios/e3.py
@@ -61,7 +61,7 @@ schematic = """
 # Process the schematic and compute the sum of part numbers
 parsed_diagram = parse_diagram(schematic)
 total_sum = sum_part_numbers(parsed_diagram)
-print(f"Total sum of part numbers: {total_sum}")
+# print(f"Total sum of part numbers: {total_sum}")
 # Scratchcards
 def scratchcards(card):
     global win 
@@ -85,7 +85,7 @@ def scratchcards(card):
             if right[i] == win[x]:
                 soma+=right[i]
     return soma 
-print(scratchcards([1,2,3,'|',1,2,3,4]))
+# print(scratchcards([1,2,3,'|',1,2,3,4]))
 # corrida de barco
 def corrida(lista_corrida):
     total_maneiras = 1  # Total de maneiras para todas as corridas
@@ -113,7 +113,7 @@ def corrida(lista_corrida):
     return total_maneiras
 
 # Testando com os dados fornecidos
-print(corrida([[7, 9], [15, 40], [30, 200]]))
+# print(corrida([[7, 9], [15, 40], [30, 200]]))
 
 import string
 
@@ -134,7 +134,7 @@ def emOrdem(alfabeto, i):
 
 # Example usage:
 alfabeto = list(string.ascii_lowercase)
-emOrdem(alfabeto, 0)
+# emOrdem(alfabeto, 0)
 
 
 #  Mirage Maintenance
@@ -154,3 +154,143 @@ def diferenca1(arr, index, diferenca):
     return diferenca1(arr, index - 1, diferenca)
 
 # print(sequencia())
+#  Calorie Counting 
+def calories(lista_caloria):
+    soma_total = []
+
+    for caloria in lista_caloria:
+        soma_individial = 0
+        for mais_caloria in caloria:
+            soma_individial+=mais_caloria
+        soma_total.append(soma_individial)
+
+    maior_caloria_elfo = 0
+    for caloria_elfo in soma_total:
+        if caloria_elfo > maior_caloria_elfo:
+            maior_caloria_elfo = caloria_elfo
+
+    return maior_caloria_elfo
+calories([[4,3,2,5],[6,3,1,3],[4,2,21]])
+
+# Rock Paper Scissors
+def rockPaperScissors():
+    mensagem = 'jogue [a=> rock] ou [b=> paper] ou [c=> scissors]]'
+    jogador1 = str(input(mensagem))
+    jogador2 = str(input(mensagem))
+    if condicao(jogador1,jogador2):
+        return rodadas(jogador1,jogador2)
+    else:
+        return rockPaperScissors()
+    
+
+def condicao(jogador1,jogador2):
+    jogadores = [jogador1,jogador2]
+    for player in jogadores:
+        if player != 'a' and player != 'b' and player != 'c':
+            return False
+        return True
+def rodadas(jogador1,jogador2):
+    if jogador1 == 'a' and jogador2 == 'c':
+        return 'game win jogador1'
+    elif jogador1 == 'a' and jogador2 == 'b':
+        return 'game win jogador2'
+    elif jogador1 == 'b' and jogador2 == 'c':
+        return 'jogador 2 win'
+    elif jogador1 == 'b' and jogador2 == 'a':
+        return 'game win jogador1 win '
+    elif jogador1 == 'c' and jogador2 == 'a':
+        return 'jogador jogador2 win'
+    elif jogador1 == 'c' and jogador2 == 'b':
+        return 'jogador1 win'
+    return None
+# print(rockPaperScissors())
+
+# Rucksack Reorganization
+def reorganization(backpacks):
+    alfabeto = list(string.ascii_lowercase)
+    return set_backpack(backpacks,alfabeto)
+def set_backpack(backpacks,alfabeto):
+    backpack = [set(),set()]
+    x = 0
+    for i in backpacks:
+        for letra in i:
+            backpack[x].add(letra)
+        x = x+1
+    mochila1 = backpack[0]
+    mochila2 = backpack[1]
+    intersection = mochila1.intersection(mochila2)
+    dict_alfabeto =  {valor: indice for indice, valor in enumerate(alfabeto)}
+    output = []
+    for i in intersection:
+        output.append(dict_alfabeto[i])
+    soma_prioridade = 0
+    for add in output:
+        soma_prioridade+=add 
+    return soma_prioridade
+
+# print(reorganization([list('vJrwpWtwJgWr'),list('hcsFMMfFFhFp')]))
+
+#  Supply Stacks 
+def supply_stack():
+    class No:
+        def __init__(self,value):
+            self.no = value 
+            self.next = None
+    class LinkedList:
+        def __init__(self):
+            self.head = None
+            self.removido = None
+            self.tail = None
+        def insertEnd(self,value):
+            if self.head == None:
+                novo_no = No(value)
+                self.head = novo_no
+            else:
+                temp = self.head
+                while(temp.next):
+                    temp = temp.next
+                novo_no = No(value)
+                temp.next = novo_no
+                self.tail = novo_no
+        def remover_end(self):
+            if self.head is None:
+                return None
+            else:
+                temp = self.head
+                while(temp.no != self.tail.no):
+                    temp = temp.next
+                if self.removido is None:
+                    removido = temp.next
+                    self.removido = removido
+                    temp.next = None
+                else:
+                    temp_removido = self.removido
+                    while(temp_removido.next):
+                        temp_removido = temp_removido.next 
+                    removido = temp.next
+                    temp_removido.next = removido
+                    temp.next = None
+    class Stack:
+        def __init__(self):
+            self.stack = LinkedList()
+        def append(self,value):
+            return self.stack.insertEnd(value)
+        def remove(self):
+            return self.stack.remover_end()
+        def mudar(self):
+            if self.stack.head is None:
+                return None
+            else:
+                return self.stack.removido
+    stack1 = Stack()
+    stack2 = Stack()
+    alfabeto = ['a','b','c','d']
+    for letra in alfabeto:
+        stack1.append(letra)
+        stack2.append(letra)
+    stack1.remove()
+    stack2.append(stack1.mudar())
+
+# supply_stack()
+        
+
