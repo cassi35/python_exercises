8c6c2ead03f2bc8dbfeeb15cae1da3c849d6401e
cassiano <cgrsobral@gmail.com>
2024-11-08 13:40:51 -0300

    comeitei depois de ter reintalado ubuntu

--- tkinker/colorChoose.py
+++ tkinker/colorChoose.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import colorchooser    
 window = Tk()
-window.geometry('420x420')
+window.geometry('100x100')
 def click():
     color = colorchooser.askcolor()
     colorHex = color[1]
