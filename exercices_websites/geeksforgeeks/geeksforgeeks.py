a3ac1eb246bed7b40080a0bcef6b1e57e32710b7
cassiano <cgrsobral@gmail.com>
2024-11-12 18:51:08 -0300

    adicinei geeksforgeeks
    coisa linda adora python

--- exercices_websites/geeksforgeeks/geeksforgeeks.py
+++ exercices_websites/geeksforgeeks/geeksforgeeks.py
@@ -12,3 +12,30 @@ arr = [2, 3, 4, 10, 40]
 x = 10
 result = binarySeach(arr,0,len(arr)-1,x)
 print(result)
+# def LeftView(self, root):
+#         if not root:
+#             return []
+        
+#         left_view = []
+#         queue = deque([root])  # Inicializa a fila com o nó raiz
+        
+#         while queue:
+#             # Número de nós no nível atual
+#             level_size = len(queue)
+            
+#             # Para cada nível, o primeiro nó é o nó da vista à esquerda
+#             for i in range(level_size):
+#                 node = queue.popleft()  # Remove o nó da fila
+                
+#                 # Se for o primeiro nó do nível, adicione à vista à esquerda
+#                 if i == 0:
+#                     left_view.append(node.data)
+                
+#                 # Adiciona os filhos à fila, se existirem
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+        
+#         return left_view
+
